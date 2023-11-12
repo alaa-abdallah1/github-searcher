5d23fae1d18898e5987e439088d72e251dce5dc6
Alaa Abdallah <alaa.abdallah.me@gmail.com>
2023-11-12 15:59:50 +0200

    feat: created ErrorMessage component

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from "react"
+import styles from "./index.module.css"
+
+interface ErrorMessageProps {
+  message?: string | null
+  isPersistent?: boolean
+}
+
+export const ErrorMessage: React.FC<ErrorMessageProps> = ({
+  message = "Something went wrong",
+  isPersistent = false,
+}) => {
+  const [visible, setVisible] = useState(true)
+
+  useEffect(() => {
+    if (!isPersistent) {
+      const timer = setTimeout(() => setVisible(false), 5000)
+      return () => clearTimeout(timer)
+    }
+  }, [message, isPersistent])
+
+  if (!visible) return null
+
+  return (
+    <div className={styles.errorMessageContainer} role="alert">
+      <div className={styles.errorMessage}>
+        {isPersistent && (
+          <button
+            className={styles.closeButton}
+            onClick={() => setVisible(false)}
+          >
+            &times;
+          </button>
+        )}
+        {message}
+      </div>
+    </div>
+  )
+}
+
+export default ErrorMessage
